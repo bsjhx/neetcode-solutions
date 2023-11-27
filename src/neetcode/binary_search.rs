889ff338a418877c56485e67fd655d949fd7e098
bsjhx <rakoczymichal@hotmail.com>
2023-11-27 21:48:13 +0100

    704. Binary Search

---
+++ src/neetcode/binary_search.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod test {
+
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut pos = nums.len() / 2;
+        let mut last_pos = pos;
+
+        let (mut l, mut r) = (0, nums.len());
+
+        while l < r {
+            let med = l + (r - l) / 2;
+            if target < nums[med] {
+                r = med;
+            } else if target > nums[med] {
+                l = med + 1;
+            } else if target == nums[med] {
+                return med as i32;
+            }
+        }
+
+        -1
+    }
+
+    #[test]
+    fn test_search() {
+        assert_eq!(search(vec![2, 5], 2), 0);
+        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12, 14], 9), 4);
+        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
+        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
+    }
+}
--- src/neetcode/mod.rs
+++ src/neetcode/mod.rs
@@ -1,3 +1,4 @@
 mod arrays_and_hashing;
+mod binary_search;
 mod stack;
 mod two_pointers;
